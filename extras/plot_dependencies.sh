b7e30123291bb6a66f09c18a62404d0bcab772b5
Henrik Andersson <jan@dhigroup.com>
2021-09-07 12:09:32 +0200

    Dependancy graph

---
+++ extras/plot_dependencies.sh
@@ -0,0 +1,4 @@
+dot -Tpng derived.dot > derived_dependancy.png
+dot -Tsvg derived.dot > derived_dependancy.svg
+dot -Tpng mikeio.dot > mikeio_dependancy.png
+dot -Tsvg mikeio.dot > mikeio_dependancy.svg
